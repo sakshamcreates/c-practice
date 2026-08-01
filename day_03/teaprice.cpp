8ca703f75a4673c8fe944f74f82fc823fb90a2d3
sakshamcreates <sakshamsingh4663@gmail.com>
2026-08-01 20:51:37 +0530

    teaprice.cpp

--- day_03/teaprice.cpp
+++ day_03/teaprice.cpp
@@ -4,7 +4,7 @@ int main(){
     float teaprice=49.99;
     int roundedteaprice=(int)teaprice;
     int teaquantity=2;
-    int totalprice=teaprice*teaquantity;
-    cout<<totalprice<<endl;
+    int totalpriceoftea=teaprice*teaquantity;
+    cout<<totalpriceoftea<<endl;
     return 0;
 }
