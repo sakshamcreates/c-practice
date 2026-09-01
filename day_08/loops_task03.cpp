a54db4ed3679d18ded6bfcfe0db324e03616048b
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-01 06:39:36 +0530

    add loops_task03

---
+++ day_08/loops_task03.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int teacups=10;
+
+    for(int i= 1; i<=teacups; i++){
+
+        cout<<"brewing cup" <<i<< "of tea....." <<endl;
+    }
+    cout<<"your tea is brewed";
+    return 0;
+}
