747dadb0397e839ed182e8cfdcfa883263dfb423
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-02 20:03:35 +0530

    add function_02

---
+++ day_09/function_02.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int globalchaistock = 100;
+
+void pourchai(int & cups){
+    cups = cups+5;
+    cout<<"poured cups: "<<cups<<endl;
+}
+//declaration of function
+
+void servechai(int cups);
+
+void makechai(){
+    cout<<"boilingwater,adding tea leaves,straining...";
+
+}
+
+void servechai(string teatype = "masala tea"){
+    int cups = 4;
+    cout<<"serving"<<teatype<<endl;
+}
