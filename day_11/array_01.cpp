7a452adb0c5c7bf3dce954a6642d70d414c30bd9
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-04 10:38:11 +0530

    array practice for Day 11

---
+++ day_11/array_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int chaiTemperature[5]= {85,90,100,110,50};
+
+    cout<<"chaiTemperature: ";
+     
+     for (int i = 0; i<5; i++)
+     {
+        cout<<chaiTemperature[i]<<"degree C\n";
+
+     }
+     return 0;
+     
+
+}
