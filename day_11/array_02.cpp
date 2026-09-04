4b9acea9368704ab60a80bf0e0a63d06930403f5
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-04 10:55:20 +0530

    array_02

---
+++ day_11/array_02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    int chaiServed [7] ={50,90,30,50,100,30,24};
+
+    cout<<"chai cups served on day 1: \n "<<chaiServed[0];
+    cout<<"chai cuups served on day 2: \n"<<chaiServed[1];
+
+  cout<<"chai cups served on day 3: \n"<<chaiServed[2];
+  cout<<"chai cups served on day 4: \n"<<chaiServed[3];
+  cout<<"chai cups served on day 5: \n"<<chaiServed[4];
+  cout<<"chai cups served on day 6: \n"<<chaiServed[5];
+ return 0;
+}
+
+
+
+
+
