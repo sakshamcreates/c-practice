be95c5133946e9f68060a6ae8a84cac2949436ca
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-04 11:46:28 +0530

    array practise for day_11

---
+++ day_11/array_04.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int* preparechaiorders(int cup){
+    int* orders = new int [cup];
+     for (int i = 0; i < cup; i++)
+     {
+        orders[i]=(i+1)*10;
+
+     }
+     return orders;
+}
+int main(){
+    int cup = 5;
+    int* chaiorders=preparechaiorders(cup);
+
+    for (int i = 0; i < cup; i++)
+    {
+        cout<<"cup:"<<i+1<<"has"<<chaiorders[i]<<"ml \n" ;
+    }
+    return 0;
+}
