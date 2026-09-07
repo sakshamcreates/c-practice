03d49597160c8f0110302324374ca6523298c68f
sakshamcreates <sakshamsingh4663@gmail.com>
2026-09-07 21:50:18 +0530

    abstractor

---
+++ day_14/abstractor.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include <string>
+using namespace std;
+
+
+
+
+class Tea {
+    public:
+    virtual void prepareingredients() = 0; //purevirtual function
+    virtual void brew() = 0;  //pure virtual function
+     virtual void serve() = 0; //pure virtual function
+
+
+    void makeTea(){
+        prepareingredients();
+        brew;
+        serve;
+    }
+
+};
+
+//derived class
+
+class GreenTea :public Tea{
+public:
+
+ void prepareingredients()override{
+    cout<<"green leaves and water is ready"<<endl;
+ }
+
+ void brew()override{
+    cout <<"tea brewed"<<endl;
+ }
+ void serve()override{
+    cout<<"tea served"<<endl; 
+ }
+
+};
